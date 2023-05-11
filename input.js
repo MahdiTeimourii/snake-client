86e3e242fd46655252c4b162a61b94c77080a37e
Mahdi Teimouri <mahditei@mail.com>
2023-05-10 21:53:36 -0700

    dsada

--- input.js
+++ input.js
@@ -1,14 +1,30 @@
-const setupInput = function () {
+let connection;
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   return stdin;
 };
-const handleUserInput = function (key) {
-  if (key === '\u0003') { // Check for Ctrl+C input
-    process.exit(); // Terminate the game
+//const handleUserInput = function (key) {
+//if (key === '\u0003') { // Check for Ctrl+C input
+//process.exit(); // Terminate the game
+//} ommited
+stdin.on("data", (key) => {
+  if (key === "w") {
+    console.log("Move: up");
+  } else if (key === "a") {
+    console.log("Move: left");
+  } else if (key === "s") {
+    console.log("Move: down");
+  } else if (key === "d") {
+    console.log("Move: right");
+  } else if (key === "\u0003") {
+    process.exit(); // Terminate the game on Ctrl+C input
   }
-};
+
+});
+
 
 module.exports = { setupInput };
