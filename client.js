db88bb0f8302869bce37a96dc8d81995aa75ebca
Mahdi Teimouri <mahditei@mail.com>
2023-05-10 20:52:16 -0700

    sdada

--- client.js
+++ client.js
@@ -1,12 +1,27 @@
+
+const net = require("net");
+
+// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '172.26.131.90',
+    host: '172.26.131.s90',
     port: 50541,
   });
   conn.on('connect', () => {
     console.log('connection is successfully established.!');
-    conn.write('Name: ___');
+    conn.write('Name: MTT');
+    // conn.write('Move: up'); // Send the "Move: up" command
+    //conn.write('Move: down');
+    //conn.write('Move: left');
+    //conn.write('Move: right');
   });
 
-  module.exports = { connect };
-};
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log("you ded cuz you idled", data);
+    return conn;
+  });
+}
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,24 +1,4 @@
 const { connect } = require('./client');
-const net = require("net");
+connect();
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '172.26.131.90',
-    port: 50541,
-  });
-  conn.on('connect', () => {
-    console.log('connection is successfully established.!');
-    conn.write('Name: ___');
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log("you ded cuz you idled", data);
-    return conn;
-  });
-
-  console.log("Connecting ...");
-  connect();
-};
